2c04d56feabca225247d236908ee84edb8126e82
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2020-03-23 23:18:02 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css";>
+    <link rel="stylesheet" href="./assets/bootstrap-4.4.1-dist/css//bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="index.php">HOME</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link" href="#"><h6>TENTANG KAMI</h6></a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#"><h6>FASILITAS</h6></a>
+        </li>    
+        <li class="nav-item">
+        <a class="nav-link" href="#"><h6>JADWAL PRAKTEK</h6></a>
+        </li>
+        <li class="nav-item text-right">
+        <a class="nav-link" href="#"><h6>LAYANAN & TARIF</h6></a>
+        </li>
+        <li class="nav-item text-right">
+        <a class="nav-link" href="#"><h6>INFO/PROMO</h6></a>
+        </li>
+        <li class="nav-item text-right">
+        <a class="nav-link" href="#"><h6>HUBUNGI KAMI</h6></a>
+        </li>
+        <li class="nav-item text-right">
+        <a class="nav-link" href="#"><h6>DAFTAR</h6></a>
+        </li>
+    </ul>
+    </div>  
+    </nav>
+
+    <div class="jumbotron text-center bg-info">
+    <h1 class="judul" style="color:white">AASIYAH MEDICAL CLINIC</h1>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="./asset/bootstrap-4.4.1-dist/js/bootsrtap.js"></script>
+</body>
+</html>
