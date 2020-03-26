bc618f0b1ace84c37100ce41b659bc6fa56f3e18
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2020-03-26 23:24:02 +0700

    second commit

--- index.php
+++ index.php
@@ -4,47 +4,86 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css";>
+    <link rel="stylesheet" type="text/css" href="home.css";>
     <link rel="stylesheet" href="./assets/bootstrap-4.4.1-dist/css//bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <a class="navbar-brand" href="index.php">HOME</a>
+    <a class="navbar-brand" href="index.php"><i class="fas fa-home"></i> HOME</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
         <li class="nav-item">
-        <a class="nav-link" href="#"><h6>TENTANG KAMI</h6></a>
+        <a class="nav-link" href="#"><h6> TENTANG KAMI </h6></a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="#"><h6>FASILITAS</h6></a>
+        <a class="nav-link" href="#"><h6> FASILITAS </h6></a>
         </li>    
         <li class="nav-item">
-        <a class="nav-link" href="#"><h6>JADWAL PRAKTEK</h6></a>
+        <a class="nav-link" href="#"><h6> JADWAL PRAKTEK </h6></a>
         </li>
-        <li class="nav-item text-right">
-        <a class="nav-link" href="#"><h6>LAYANAN & TARIF</h6></a>
+        <li class="nav-item">
+        <a class="nav-link" href="#"><h6> LAYANAN & TARIF </h6></a>
         </li>
-        <li class="nav-item text-right">
-        <a class="nav-link" href="#"><h6>INFO/PROMO</h6></a>
-        </li>
-        <li class="nav-item text-right">
-        <a class="nav-link" href="#"><h6>HUBUNGI KAMI</h6></a>
-        </li>
-        <li class="nav-item text-right">
-        <a class="nav-link" href="#"><h6>DAFTAR</h6></a>
+        <li class="nav-item">
+        <a class="nav-link" href="#"><h6> HUBUNGI KAMI </h6></a>
         </li>
     </ul>
+    <button type="button" class="btn btn-primary">DAFTAR</button>
     </div>  
     </nav>
 
     <div class="jumbotron text-center bg-info">
-    <h1 class="judul" style="color:white">AASIYAH MEDICAL CLINIC</h1>
+    <h1 class="judul display-3" style="color:white">Aasiyah Medical Clinic</h1>
     </div>
 
+    <br>
+    <div class="container">
+        <div class="card-columns">
+            <div class="card bg-primary">
+                <div class="card-body text-center">
+                <h1><i class="fas fa-heart" style="color:white"></i></h1>
+                <h3 class="card-text" style="color:white">Layanan Lengkap</h3>
+                <p class="card-text" style="color:white">Hadir dengan layanan yang lengkap dan terjamin standar kualitasnya.</p>
+                <a href="#" class="btn btn-link" style="color:white"><b>LIHAT DAFTAR LAYANAN  <i class="fas fa-chevron-right" style="color:white"></i><i class="fas fa-chevron-right" style="color:white"></i></b></a>
+                </div>
+            </div>
+            <div class="card bg-dark">
+                <div class="card-body text-center">
+                <h1><i class="fas fa-map-marker-alt" style="color:white"></i></h1>
+                <h3 class="card-text" style="color:white">Lokasi</h3>
+                <p class="card-text" style="color:white">Lihat peta untuk memudahkan anda menuju lokasi klinik.</p>
+                <a href="#" class="btn btn-link" style="color:white"><b>LIHAT LOKASI  <i class="fas fa-chevron-right" style="color:white"></i><i class="fas fa-chevron-right" style="color:white"></i></b></a>
+                </div>
+            </div>
+            <div class="card bg-danger">
+                <div class="card-body text-center">
+                <h1><i class="fas fa-hospital" style="color:white"></i></h1>
+                <h3 class="card-text" style="color:white">Hubungi Kami</h3>
+                <p class="card-text" style="color:white">Ketahui lebih lengkapnya dengan menghubungi kami.</p>
+                <a href="#" class="btn btn-link" style="color:white"><b>CONTACT  <i class="fas fa-chevron-right" style="color:white"></i><i class="fas fa-chevron-right" style="color:white"></i></b></a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <h2 class="a text-center" style="color:#135589">SELAMAT DATANG DI</h2>
+        <h3 class="b text-center" style="color:#135589">AASIYAH MEDICAL CLINIC</h3>
+        <p style="color:#135589">Klinik Darma Nusantara berintegrasi dan berfokus dalam memberikan pelayanan kesehatan, pengobatan dan pencegahan penyakit. Klinik Darma Nusantara tumbuh dari kepedulian kesehatan dan kesejahteraan karyawan perusahaan serta keluarga yang diharapkan dapat membangun perusahaan, masyarakat sekitar dan juga negara.</p>
+        <p style="color:#135589">Untuk mencapai kepedulian diatas dan juga pengembangannya, maka klinik Darma Nusantara harus terus meningkatkan kualitas pelayan kesehatan yang baik dan akan terus bertransformasi dalam mengikuti teknologi kesehatan dan juga menjalankan etika bisnis yang baik.</p>
+        <p style="color:#135589">Klinik Darma Nusantara dikemudian hari diharapakan mendapatkan dukungan oleh semua pihak yang terkait dan terus memberikan pelayanan kesehatan secara tuntas sesuai dengan visi, misi dan nilai-nilai yang dicanangkan.</p>
+    </div>
+    <br>
+    <br>
+    <br>
+
+    <div class="text-right bg-dark" style="margin-bottom:0">
+    <p style="color:white">Copyright 2020 </p>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="./asset/bootstrap-4.4.1-dist/js/bootsrtap.js"></script>
